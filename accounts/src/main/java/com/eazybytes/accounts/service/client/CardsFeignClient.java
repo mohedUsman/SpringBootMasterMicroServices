59fb4646d23bb18acb1de32327a2ab7d7c69e2a5
Mohammed2.Usman <justuseaicode@gmail.com>
2025-09-26 21:23:25 +0530

    Service Discovery & service registration: For Communication with Accounts Microservices with Cards and Loans Services for that I have added the Dependency and created the Client class Further Impl will be added in next commit

---
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/CardsFeignClient.java
@@ -0,0 +1,22 @@
+package com.eazybytes.accounts.service.client;
+
+import com.eazybytes.accounts.dto.CardsDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/*
+    * This is a Feign Client interface for communicating with the Cards microservice.
+    * and it is annotated with @FeignClient to specify the name of the service to connect to.
+    * Remember: the cards is the name of the Cards microservice registered with Eureka Server.
+    * Remember: the fetchCardsDetails method Should match with the method defined in the CardsController class in Cards microservice.
+    * I need to create a CardsDto class to hold the response from the Cards microservice we can copy the CardsDto class from the Cards microservice.
+    * and Also the Endpoint /api/fetch should match with the endpoint defined in the CardsController class in Cards microservice.
+ */
+@FeignClient("cards")
+public interface CardsFeignClient {
+
+    @GetMapping(value = "/api/fetch", consumes = "application/json")
+    public ResponseEntity<CardsDto> fetchCardsDetails(@RequestParam String mobileNumber);
+}
