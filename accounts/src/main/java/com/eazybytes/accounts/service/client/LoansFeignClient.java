c304722146bc2d775567eb47ecb879ccc9757833
Mohammed2.Usman <justuseaicode@gmail.com>
2025-09-28 23:01:43 +0530

    Service Discovery & service registration: For Communication with Accounts Microservices with Cards and Loans Services for that I have created an api with new controller class so by hitting that api it will auto hit the fatch apis of other services created the supporting responce  DTO to give the responce

--- accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/client/LoansFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /*
     * This is a Feign Client interface for communicating with the Cards microservice.
     * and it is annotated with @FeignClient to specify the name of the service to connect to.
-    * Remember: the cards is the name of the Cards microservice registered with Eureka Server.
+    * Remember: the cards should be same as the name of the Cards microservice registered with Eureka Server .
     * Remember: the fetchCardsDetails method Should match with the method defined in the CardsController class in Cards microservice.
     * I need to create a CardsDto class to hold the response from the Cards microservice we can copy the CardsDto class from the Cards microservice.
     * and Also the Endpoint /api/fetch should match with the endpoint defined in the CardsController class in Cards microservice.
